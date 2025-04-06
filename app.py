08db56e64d63362e44b17b7edba52e5680f3c178
Isanka Suresh <isankasuresh001@gmail.com>
2025-04-06 12:34:52 +0530

    implement docker

--- app.py
+++ app.py
@@ -1,60 +1,115 @@
 # pip install Flask
 
 # python --version
-
-from flask import Flask, render_template, request
-# import joblib
-# import numpy as np
-# import pandas as pd
-# import warnings
-# warnings.filterwarnings('ignore')
-
-# # Load preprocessor and model
-# preprocessor = joblib.load("preprocessor.pkl")
-# svm_model = joblib.load("svm_model.pkl")
+from flask import Flask, render_template, request, jsonify
+import joblib
+import pandas as pd
+from flask_cors import CORS # type: ignore
 
 app = Flask(__name__)
-
-# # Define categorical and numerical columns
-# categorical_features = ['job', 'marital', 'education', 'default', 'housing', 'loan', 
-#                         'contact', 'month', 'day_of_week', 'poutcome']
-# numerical_features = ['age', 'duration', 'campaign', 'pdays', 'previous', 'emp.var.rate', 
-#                       'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed']
+CORS(app)
 
 
-# @app.route('/')
-# def index():
-#     return render_template('bank-form.html')
+lr_model = joblib.load("lr_model.joblib")
+svm_model = joblib.load("svm_model.joblib")
+preprocessor = joblib.load("preprocessor.joblib")
 
-# @app.route('/predict', methods=['POST'])
-# def customer_details():
-#     form = request.form
-#     manual_input = pd.DataFrame([{
-#         'age': form['age'], 'duration': np.nan, 'campaign': np.nan, 'pdays': np.nan, 'previous': np.nan,
-#         'emp.var.rate': np.nan, 'cons.price.idx': np.nan, 'cons.conf.idx': np.nan,
-#         'euribor3m': np.nan, 'nr.employed': np.nan,
-#         'job': form['job'], 'marital': form['marital'], 'education': form['education'], 'default': form['default'],
-#         'housing': form['housing'], 'loan': form['loan'], 'contact': np.nan, 'month': np.nan,
-#         'day_of_week': np.nan, 'poutcome': np.nan
-#     }])
+categorical_features = [
+    "job",
+    "marital",
+    "education",
+    "default",
+    "housing",
+    "loan",
+    "contact",
+    "month",
+    "day_of_week",
+    "poutcome",
+]
+numerical_features = [
+    "age",
+    "campaign",
+    "pdays",
+    "previous",
+    "emp.var.rate",
+    "cons.price.idx",
+    "cons.conf.idx",
+    "euribor3m",
+    "nr.employed",
+]
 
-#     manual_input[numerical_features] = manual_input[numerical_features].fillna(0)
-#     manual_input[categorical_features] = manual_input[categorical_features].fillna('unknown')
 
-#     manual_transformed = preprocessor.transform(manual_input)
-#     prediction = svm_model.predict(manual_transformed)
-    
-#     # return the predicted output as a response
-#     return render_template('dashboard.html', predict=prediction[0])
+def prepare_input(data):
+    return pd.DataFrame(
+        [
+            {
+                "age": int(data["age"]),
+                "job": data["job"],
+                "marital": data["marital"],
+                "education": data["education"],
+                "default": data["default"],
+                "housing": data["housing"],
+                "loan": data["loan"],
+                "contact": "cellular",
+                "month": "may",
+                "day_of_week": "mon",
+                "campaign": 1,
+                "pdays": 0,
+                "previous": 0,
+                "duration": int(data.get("duration", 200)),
+                "poutcome": "nonexistent",
+                "emp.var.rate": 1.1,
+                "cons.price.idx": 93.994,
+                "cons.conf.idx": -36.1,
+                "euribor3m": 4.857,
+                "nr.employed": 5191.0,
+            }
+        ]
+    )
 
-# if __name__ == '__main__':
-#     # app.run(debug=True)
-#     app.run(host="0.0.0.0", port=80) # to change the port .
+def getPredict(val):
+    if(val == 1): return 'Yes'
+    return 'No'
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    return '<h1>AI v01</h1>'
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80) # to change the port .
+
+@app.route("/svm")
+def svm_page():
+    return render_template("svmModel.html")
+
+
+@app.route("/lrm")
+def lrm_page():
+    return render_template("IrModel.html")
+
+
+@app.route("/svm-predict", methods=["POST"])
+def predict_svm():
+    data = request.form
+
+    input_df = prepare_input(data)
+
+    manual_transformed = preprocessor.transform(input_df)
+    prediction = svm_model.predict(manual_transformed)
+
+    return render_template("predict.html", predict=getPredict(prediction[0]))
+
+
+@app.route("/lrm-predict", methods=["POST"])
+def predict_lrm():
+    data = request.form
+
+    input_df = prepare_input(data)
+
+    manual_transformed = preprocessor.transform(input_df)
+    prediction = lr_model.predict(manual_transformed)
+
+    return render_template("predict.html", predict=getPredict(prediction[0]))
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)  # to change the port .
